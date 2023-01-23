dee33a7de40880f5cc34a7044de6cf22304bd552
Allan Luo <allanluo@gmail.com>
2023-01-23 11:42:31 +0100

    name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to my Homepage
+          Allan Luo
         </a>
       </header>
     </div>
