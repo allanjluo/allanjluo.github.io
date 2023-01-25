55de291bbdb7500a31f381d986a4c5abae9a895a
Allan Luo <allanluo@gmail.com>
2023-01-25 02:28:04 +0000

    page added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Allan Luo
+          Allan Luo's Page
         </a>
       </header>
     </div>
